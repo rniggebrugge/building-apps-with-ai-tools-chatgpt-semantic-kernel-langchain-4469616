a37de7a10627bb61d9c7f6a44618c30cc11312bd
Remco Niggebrugge <remco.niggebrugge@gmail.com>
2023-12-12 11:17:28 +0000

    added files

--- src/main.py
+++ src/main.py
@@ -4,3 +4,17 @@ import openai
 from dotenv import load_dotenv
 load_dotenv()
 openai.api_key = os.getenv('OPENAI_API_KEY')
+
+files = ["lost_debit_card.wav","sample-1.mp3","my_audio.wav"]
+
+for input_filename in files:
+  print(input_filename)
+  with open(Path.cwd()/input_filename,"rb") as audio_file:
+    transcript = openai.Audio.transcribe(
+      model="whisper-1",
+      file=audio_file
+    )
+    print(transcript)
+     
+
+  print("- "*40)
