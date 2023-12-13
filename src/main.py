7a054e845426672cb75f7db7fae79940fb765021
Remco Niggebrugge <remco.niggebrugge@gmail.com>
2023-12-13 11:26:32 +0000

    embeddings...

--- src/main.py
+++ src/main.py
@@ -1,1 +1,14 @@
 from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import WebBaseLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import FAISS
+from langchain.embeddings.openai import OpenAIEmbeddings
+
+loader = WebBaseLoader("https://en.wikipedia.org/wiki/Tea")
+documents = loader.load()
+
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+texts = text_splitter.split_documents(documents)
+
+embeddings = OpenAIEmbeddings()
+docsearch = FAISS.from_documents(texts, embeddings)
