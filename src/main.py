a1b3bde1cad63600dde9ecb956ebf16e484acfd4
Remco Niggebrugge <remco.niggebrugge@gmail.com>
2023-12-19 15:46:38 +0000

    typed in the code

--- src/main.py
+++ src/main.py
@@ -4,3 +4,31 @@ from langchain.prompts.chat import (
 )
 from langchain.output_parsers import PydanticOutputParser
 from pydantic import BaseModel, Field
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts.chat import (
+  PromptTemplate
+)
+from langchain.output_parsers import PydanticOutputParser
+from pydantic import BaseModel, Field
+
+class Furniture(BaseModel):
+  type: str = Field(description="the type of furniture")
+  style: str = Field(description="the style of furniture")
+  colour: str = Field(description="colour")
+
+furniture_request = "I'd like a blue mid centure chair"
+
+parser = PydanticOutputParser(pydantic_object=Furniture)
+
+prompt = PromptTemplate(
+  template="Answer the user query.\n{format_instructions}\n{query}",
+  input_variables=["query"],
+  partial_variables={"format_instructions": parser.get_format_instructions()}
+  )
+
+_input = prompt.format_prompt(query=furniture_request)
+model = ChatOpenAI()
+output = model.predict(_input.to_string())
+parsed = parser.parse(output)
+
+print(parsed)
