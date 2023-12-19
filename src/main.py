6529393c15ed7e4781904dc24527f986c3686885
Remco Niggebrugge <remco.niggebrugge@gmail.com>
2023-12-19 15:28:49 +0000

    while loop

--- src/main.py
+++ src/main.py
@@ -3,6 +3,11 @@ from langchain.document_loaders import WebBaseLoader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import FAISS
 from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.chains import RetrievalQA
+from langchain.llms import OpenAI
+
+from dotenv import load_dotenv
+load_dotenv()
 
 loader = WebBaseLoader("https://en.wikipedia.org/wiki/Tea")
 documents = loader.load()
@@ -12,3 +17,12 @@ texts = text_splitter.split_documents(documents)
 
 embeddings = OpenAIEmbeddings()
 docsearch = FAISS.from_documents(texts, embeddings)
+
+qa = RetrievalQA.from_chain_type(llm=OpenAI(), chain_type="stuff", retriever=docsearch.as_retriever())
+
+while True:
+  print("./-"*30)
+  query = input("Ask a question about tea (type 'stop' to exit program)\n")
+  if query == 'stop':
+    break
+  print(qa.run(query))
